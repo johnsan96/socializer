3d3e8ef50d8de5c7581589d71a79d8c15705374d
john <john@getcoding.de>
2024-04-01 00:43:23 +0200

    finalize login

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,27 +1,58 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
+const model = require("../models/user")
+
+/* export async function getProjectById(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+        const project = await Project.findByPk(id);
+        if (!project) {
+            return res.status(404).json({ error: 'Projekt nicht gefunden' });
+        }
+        res.json(project);
+    } catch (error) {
+        console.error('Fehler beim Abrufen des Projekts:', error);
+        res.status(500).json({ error: 'Fehler beim Abrufen des Projekts' });
+    }
+} */
 
 function generateAccessToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 }
+
 exports.login = (req, res) => {
+    const name = req.body.name;
+    const password = req.body.password;
 
-    const username = req.body.name
-    const password = req.body.password
-    const user = { name: username, password: password }
-    
+    model.findOne({
+        where: {
+            username: name
+        }
+    })
+        .then(foundUser => {
+            if (!foundUser) {
 
+                return res.status(401).json({ message: "Benutzer existiert nicht." });
+            }
 
-    const accessToken = generateAccessToken(user)
-    /*  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-     refreshTokens.push(refreshToken) */
+            bcrypt.compare(password, foundUser.password)
+                .then(result => {
+                    if (!result) {
 
-    res.cookie("token", accessToken,
-        {
-            httpOnly: false
+                        return res.status(401).json({ message: "Falsches Passwort." });
+                    }
+
+                    const accessToken = generateAccessToken({ id: foundUser.id, username: foundUser.username });
+                    res.cookie("token", accessToken, { httpOnly: false });
+                    res.json({ accessToken: accessToken });
+                });
         })
-    res.json({ accessToken: accessToken/* , refreshToken: refreshToken  */})
+        .catch(error => {
+            console.error('Fehler beim Login:', error);
+            res.status(500).json({ error: 'Interner Serverfehler' });
+        });
 }
+
 
 const token = (req, res) => {
     const refreshToken = req.body.token
